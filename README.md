238ec183d5491770a80b5de6c664db1871ebde68
Adithya Pai B <paiadithya26@gmail.com>
2024-06-11 20:27:46 +0530

    Updated with MongoDB

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
-# A Workshop on Express JS
 
-[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+ ![Internet](https://img.icons8.com/fluency/48/000000/internet.png) 
+ 
+
+# SUS (SMOL URL SHORTENER)[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+
+
 
 ## Description
+SUS is a simple URL shortener that allows users to shorten long URLs. The application is built using Node.js, Express, and MongoDB. The application provides a simple user interface where users can enter a long URL and get a shortened version of the URL. The shortened URL can be used to redirect to the original URL. The application also provides a list of all the shortened URLs created by the user. The application uses a unique ID to generate the shortened URL and stores the original URL and the shortened URL in a MongoDB database. The application also provides a simple analytics dashboard that shows the number of times the shortened URL has been used.
 
-Learn Express JS and build a simple REST API
+
+## Features
+- Shorten long URLs
+- Redirect to original URL using shortened URL
+- View list of shortened URLs
+
+
+## Technologies
+- Node.js
+- Express
+- MongoDB
+- Mongoose
 
 
 
@@ -12,7 +29,7 @@ Learn Express JS and build a simple REST API
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Contributing](#contributing)
+- [API Endpoints](#api-endpoints)
 - [License](#license)
 
 ## Installation
@@ -56,6 +73,32 @@ To get started with the workshop, follow these steps:
     ```
 5. Open your browser and navigate to `http://localhost:3000/` to see the application in action.
 
+
+## Usage
+
+To use the application, follow these steps:
+
+1. Open your browser and navigate to `http://localhost:3000/`.
+
+2. Enter a long URL in the input field and click the `Shorten` button to generate a shortened URL.
+
+3. Copy the shortened URL and use it to redirect to the original URL.
+
+
+## API Endpoints
+
+The application provides the following API endpoints:
+
+1. `GET /log`:  Get all the shortened URLs created by the user.
+2. `GET / `:  Front end of the application
+3. `POST /savedata`:  Save the shortened URL in the database.
+4. `GET /:id`:  Redirect to the original URL using the shortened URL.
+5. `GET /*`:  Get the analytics data for the shortened URL.
+
+
+## License
+
+This project is licensed under the MIT license. For more information, see the [LICENSE](LICENSE) file.
 
 
 
--- index.js
+++ index.js
@@ -1,23 +1,72 @@
 // Importing the required modules
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = 3000;
+
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/urlshortener');
+
+// Create a schema
+
+const urlSchema = new mongoose.Schema({
+    shortid: String,
+    url: String
+});
+
+// Create a model
+const Model = mongoose.model('Url', urlSchema);
 
 
 // Middleware to parse the request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static('static'));
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+//  Rendering the index.html file
+app.get("/", (req, res) => res.sendFile(__dirname + "/static/index.html"));
+
+
+// Displaying the data in the database
+app.get("/log", (req, res) => Model.find({}).then((data) => res.json(data)));
+
+
+// Saving the data in the database
+app.post("/savedata", async (req, res) => {
+    const { shortid, url } = req.body;
+    let shortIdExists = false;
+    await Model.find({ shortid }).then((data) => {
+        if (data.length > 0) {
+            res.send(`<body bgcolor=red> Shortid already exists </body> <script>setTimeout(() => {window.location.href = "/";}, 2000);</script>`);
+            shortIdExists = true;
+            return;
+        }
+
+    });
+    if (shortIdExists)
+        return;
+
+    if (!shortid || !url) {
+        return res.send(`<body bgcolor=red> Data Not Entered </body> <script>setTimeout(() => {window.location.href = "/";}, 2000);</script>`);
+    }
+
+    if (!/^[a-zA-Z0-9]+$/.test(shortid)) {
+        return res.send(`<body bgcolor=red> Shortid should be alphanumeric </body> <script>setTimeout(() => {window.location.href = "/";}, 2000);</script>`);
+    }
+
+    // Inserting the data into the database
+    Model.create({ shortid, url }).then(() => { console.log("Data Entered Successfully"); });
+    res.send(`<body bgcolor=green> Data Entered SuccessFully </body> <script>setTimeout(() => {window.location.href = "/";}, 2000);</script>`)
 });
 
+// Redirecting to the 404 page
+app.get("/404", (req, res) => { res.send(`<img src="https://c.tenor.com/q2eL6vNVKf4AAAAC/tenor.gif">`); });
 
 
+// Redirecting to the original url
+app.get("/:shortid", async(req, res) => await Model.findOne({ shortid: req.params.shortid }).then((data) => res.redirect(data != null? data.url : "/404")));
 
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+// Listening to the port
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
